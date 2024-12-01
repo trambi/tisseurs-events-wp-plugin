347de15293759e4f481e7686f27adf5b94968b37
Bertrand Madet <bertrand.madet@protonmail.com>
2024-12-01 14:22:28 +0100

    :alembic: Add phpUnit and BrainMonkey as suggested in wp-punk.com article
    https://wp-punk.com/the-practice-of-wordpress-unit-testing/#Brain_Monkey_environment_setup

---
+++ tests/DbUtilsTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class DbUtilsTest extends TestCase
+{
+    public function testFailure(): void
+    {
+        // // Given
+        // $forTest = $this->createStub(wpdb);
+        // $forTest->prefix = 'wp';
+        // $excepted = 'wp_tes_room';
+        // // When
+        // $having = get_room_table_name($forTest);
+        // // Then
+        // $this->assertEquals(
+        //     $excepted,
+        //     $having
+        // );
+        $this->assertTrue(true);
+    }
+}
