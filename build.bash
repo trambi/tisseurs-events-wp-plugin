f8cc42ee821de82ec011cfecd30cf848584b70fa
Bertrand Madet <bertrand.madet@protonmail.com>
2024-12-02 02:27:24 +0100

    :recycle: Use CONSTANT in bash

--- build.bash
+++ build.bash
@@ -1,16 +1,16 @@
 # Script bash to create the downloadable archive of this WordPress plugin
-
+readonly PLUGIN_NAME="tisseurs-events-scheduler"
 if [ ! -d dist ]; then
   mkdir dist
 fi
-rm -rf dist/tisseurs-events-wp-plugin.zip
-workingdir=$(mktemp -d)
-mkdir ${workingdir}/tisseurs-events-wp-plugin
-cp -r src/* ${workingdir}/tisseurs-events-wp-plugin
-currentdir=$(pwd)
-cd ${workingdir}
-zip -r tisseurs-events-wp-plugin.zip tisseurs-events-wp-plugin/*
-mv tisseurs-events-wp-plugin.zip ${currentdir}/dist
-cd ${currentdir}
-rm -rf ${workingdir}
+rm -rf dist/${PLUGIN_NAME}.zip
+readonly WORKING_DIR=$(mktemp -d)
+mkdir ${WORKING_DIR}/${PLUGIN_NAME}
+cp -r src/* ${WORKING_DIR}/${PLUGIN_NAME}
+CURRENT_DIR=$(pwd)
+cd ${WORKING_DIR}
+zip -r ${PLUGIN_NAME}.zip ${PLUGIN_NAME}/*
+mv ${PLUGIN_NAME}.zip ${CURRENT_DIR}/dist
+cd ${CURRENT_DIR}
+rm -rf ${WORKING_DIR}
 
